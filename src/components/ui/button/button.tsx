750b7d8e17b6d09003d45c8044732ff940bfc08c
NMakarevich <nikoladze529@mail.ru>
2025-02-04 15:14:30 +0300

    refactor: update button props

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -3,15 +3,18 @@ import './button.scss';
 
 interface Props {
   title: string;
+  disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
   handleClick: () => void;
 }
 
 function Button(props: Props): React.ReactNode {
   const { title, handleClick } = props;
+  const type = props.type ?? 'button';
 
   return (
     <>
-      <button className={'button'} type="button" onClick={handleClick}>
+      <button className={'button'} type={type} onClick={handleClick}>
         {title}
       </button>
     </>
