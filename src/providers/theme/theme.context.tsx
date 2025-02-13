2038a0bbf28275e36c40ebff7ac4fd4456cdee6e
NMakarevich <nikoladze529@mail.ru>
2025-02-13 06:55:50 +0300

    fix: fix context default value

--- src/providers/theme/theme.context.tsx
+++ src/providers/theme/theme.context.tsx
@@ -2,6 +2,6 @@ import { createContext } from 'react';
 import { IThemeContext } from '../../interfaces.ts';
 
 export const ThemeContext = createContext<IThemeContext>({
-  theme: 'dark',
-  setTheme: (theme) => theme,
+  isSwitched: false,
+  setIsSwitched: (isSwitched: boolean) => isSwitched,
 });
