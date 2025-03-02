cd0b74d53b927ec5771732fb93b9bde3e317da90
NMakarevich <nikoladze529@mail.ru>
2025-03-02 15:14:11 +0300

    refactor: update project structure

--- pages/search/[...resource]/index.tsx
+++ pages/search/[...resource]/index.tsx
@@ -1,10 +1,17 @@
-import { ReactNode } from 'react';
-import Layout from '../../layout';
+import React from 'react';
 import Main from '../../../src/Main';
 import { API_ENDPOINTS, RESOURCES } from '../../../src/constants';
 import { DetailedResponse, Response } from '../../../src/interfaces';
 import { GetServerSidePropsContext } from 'next';
 import NotFoundPage from '../../404';
+import { store } from '../../../src/redux/store';
+import ThemeProvider from '../../../src/providers/theme/theme.provider';
+import styles from '../../../src/App.module.scss';
+import SelectResource from '../../../src/components/selectResource/selectResource';
+import Search from '../../../src/components/search/search';
+import ThemeToggle from '../../../src/components/theme-toggle/theme-toggle';
+import Flyout from '../../../src/components/flyout/flyout';
+import { Provider } from 'react-redux';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const params = context.params
@@ -38,14 +45,29 @@ function Page(props: {
   data: Response;
   detailed: DetailedResponse;
   error: string;
-}): ReactNode {
+}): React.ReactNode {
   const { data, detailed, error } = props;
   if (error) return <NotFoundPage />;
 
   return (
-    <Layout>
-      <Main data={data} detailed={detailed} />
-    </Layout>
+    <Provider store={store}>
+      <ThemeProvider>
+        <header className={`${styles['app-header']}`}>
+          <div className={styles.container}>
+            <SelectResource />
+            <Search />
+            <ThemeToggle />
+          </div>
+        </header>
+        <main className={`${styles['app-main']}`}>
+          <div className={styles.container}>
+            {' '}
+            <Main data={data} detailed={detailed} />
+          </div>
+        </main>
+        <Flyout />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
